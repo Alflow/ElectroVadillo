6dcdb33380bc7bb8957864e76c0d0d5bf5589cef
Alfredo Alberruche Herrero <aalberrh@gmail.com>
2024-02-01 12:37:04 +0100

    header and footer improved

--- app/plantillas/inicio.php
+++ app/plantillas/inicio.php
@@ -1,20 +1,82 @@
 <?php
-/* Ejemplo de plantilla que se mostrará dentro de la plantilla principal
-  ob_start() activa el almacenamiento en buffer de la página. Mientras se
-             almacena en el buffer no se produce salida alguna hacia el
-             navegador del cliente
-  luego viene el código html y/o php que especifica lo que debe aparecer en
-     el cliente web
-  ob_get_clean() obtiene el contenido del buffer (que se pasa a la variable
-             $contenido) y elimina el contenido del buffer
-  Por último se incluye la página que muestra la imagen común de la aplicación
-    (en este caso base.php) la cual contiene una referencia a la variable
-    $contenido que provocará que se muestre la salida del buffer dentro dicha
-    página "base.php"
-*/
+
  ?>
 <?php ob_start() ?>
 
+<div class="row row-cols-3 row-cols-md-6 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
 
  <?php $contenido = ob_get_clean() ?>
 
